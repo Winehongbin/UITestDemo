5cb220e3fb52bf4d55fab6a24955b93a0c491b8f
youmeizhi <951142130@qq.com>
2018-05-03 15:52:42 +0800

    youmeizhi

---
+++ pages/wechat/create_material.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from pages.common_pages.base import BasePage
+from pages.common_pages.driver import brower
+from pages.common_pages.base import LoginPage
+from pages.common_pages.base import ChoosePage
+import time
+class Create_material():
+
+    def create():
+        driver=brower()
+        driver.get
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
