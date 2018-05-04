dfe127dff7bf7de564f6fce35cd9d12b8d07d2ab
高明 <cara_gao@sinobasedm.com>
2018-05-04 11:29:00 +0800

    最新模板测试

--- pages/common_pages/login_page.py
+++ pages/common_pages/login_page.py
@@ -7,11 +7,8 @@ from selenium.webdriver.common.by import By
 class LoginPage(BasePage):
     def login(self):
         self.open()
-        self.driver.implicitly_wait(30)
-        # self.find_element_input('x','/html/body/div[2]/div/div/div[1]/form/div[1]/input','18210127910')
-        self.find_element_input('x','//*[@id="login"]/form/div[1]/input','18210127910')
-
-        # self.find_element_input('css',"input[type='password']","123123")
+        self.find_element_input('x','/html/body/div[2]/div/div/div[1]/form/div[1]/input','18210127910')
+        self.find_element_input('css',"input[type='password']","123123")
         #点击登录按钮
         self.element_click('x','/html/body/div[2]/div/div/div[1]/form/input')
         self.driver.implicitly_wait(30)
