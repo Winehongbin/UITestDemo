bf1d8be349d2aacbfa0fde07466e86bf4b7e2553
高明 <cara_gao@sinobasedm.com>
2018-05-07 15:00:56 +0800

    report最新样式

--- auto_test_control.py
+++ auto_test_control.py
@@ -19,6 +19,13 @@ import time
 from common.mail import email_oper
 
 class ConrollerShow():
+
+    """
+    def_list 获取单元测试中，测试函数列表
+    dir():返回当前范围内的变量、方法和定义的类型列表
+    str(tmp)[:4] :索引和切片，从下标为0的元素选择到下标为3的元素，不包括下标4的元素
+    append() 方法向列表的尾部添加一个新的元素。只接受一个参数
+    """
     def Def_List(self,class_name):
         #class的名字，不用双引号，直接用
         list = []
@@ -28,31 +35,43 @@ class ConrollerShow():
             if def_four == "test":
                 list.append(tmp)
         return list
+
+    """
+    SupportTool_Control 用来管理我们的用例启动方式，执行所有配置好的单元测试，生成报告并发送
+    time()：返回当前时间的时间戳（1970纪元后经过的浮点秒数），需要import time
+    """
     def SupportTool_Control(self):
         StartTime = time.time()
-        suite = unittest.TestSuite()
+        suite = unittest.TestSuite()     #创建一个测试集合
 
-        #线下会   #文件名： #类名：
-        offline_test = self.Def_List(Offline_Meeting_Test)
+
+        offline_test = self.Def_List(Offline_Meeting_Test)   #Def_List 获取指定单元测试中，测试函数列表
         for offline_tmp in offline_test:
-            suite.addTest(Offline_Meeting_Test(offline_tmp))
+            suite.addTest(Offline_Meeting_Test(offline_tmp))   #addTest()的方法，测试套件中添加测试用例,可以加载不同类里面的不同测试函数
 
-        #创建测试报告
+        """
+        创建测试报告
+        AddSuite = report.AllReport() :实例化AllReport类
+        OnlyNeed_Suite(suite) ：指定suit的report
+
+        """
         AddSuite = report.AllReport()
         AddSuite.OnlyNeed_Suite(suite)
 
         # 发送邮件
         EndTime = time.time()
         PerformTime = EndTime - StartTime
-        # content = "autoTest"
-        content ="test_002_createoffline"
+        content = "autoTest"
+        # content ="test_002_createoffline"
         # content =["test_001_loginoffline","test_002_createoffline","test_003_interaction"]
 
         SendEmail = email_oper.SendEmailModel()
-        SendEmail.PostReport_only(PerformTime,str(content))
+        SendEmail.PostReport_only(PerformTime,str(content))  #20180507
+
 
 if __name__ == '__main__':
     A = ConrollerShow()
+
     A.SupportTool_Control()
 
 
