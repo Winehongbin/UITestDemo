9835e08c3c00b1f2db33e5114f874603f1a4bda5
李鸿飞 <498340427@qq.com>
2018-05-10 17:18:32 +0800

    修改了新的输入打印，本地调试完毕了方法

--- pages/member/member_management.py
+++ pages/member/member_management.py
@@ -8,37 +8,38 @@ class FieldAction(BasePage):
     def new_custom_mail_field(self):
         self.element_click("id","_settingFelid")#点击属性/字段设置
         self.element_click("css","#con-basefield > div.m-tit-bar > div > div > button")#点击新增字段
-        print u"点击新增字段"
+        time.sleep(1)
+        self.deprint(u"点击新增字段")
         self.element_value_input("x",'//*[@id="setFieldModal"]/div/div/div[2]/form/div[1]/div/input',u"自定义邮箱身份标识")#输入字段中文名称
-        print u"输入字段中文名称：自定义邮箱身份标识"
+        self.deprint(u"输入字段中文名称：自定义邮箱身份标识")
         self.element_value_input("x","//*[@id='setFieldModal']/div/div/div[2]/form/div[2]/div/input",u"zidingyiEmail")#输入字段英文名称
-        print u"输入字段中文名称：zidingyiEmail"
+        self.deprint(u"输入字段中文名称：zidingyiEmail")
         self.element_click("x","//*[@id='setFieldModal']/div/div/div[2]/form/div[3]/div/div/button")#选择字段类型
         self.element_click("x","//*[@id='setFieldModal']/div/div/div[2]/form/div[3]/div/div/ul/li[2]")#选择文本字段
-        print u"字段类型：选择文本字段"
+        self.deprint(u"字段类型：选择文本字段")
         self.element_click("x","//*[@id='setFieldModal']/div/div/div[2]/form/div[5]/div/div[1]/div/label")#选择身份标识为“是”
-        print u"是否是身份标识，选择：是"
+        self.deprint(u"是否是身份标识，选择：是")
         self.element_click("x","//*[@id='setFieldModal']/div/div/div[2]/form/div[5]/div/div[5]/div/button")#点击校验规则
         self.element_click("x","//*[@id='setFieldModal']/div/div/div[2]/form/div[5]/div/div[5]/div/ul/li[2]/a")#选择预设校验
         self.element_click("x","//*[@id='setFieldModal']/div/div/div[2]/form/div[5]/div[1]/div[6]/div/button")#选择预设校验类型
         self.element_click("x","//*[@id='setFieldModal']/div/div/div[2]/form/div[5]/div[1]/div[6]/div/ul/li[2]/a")#选择邮箱类型预设校验
-        print u"字段校验规则选择：邮箱预设校验"
+        self.deprint(u"字段校验规则选择：邮箱预设校验")
         self.element_click("x","//*[@id='setFieldModal']/div/div/div[3]/button")#点击保存，保存创建的字段
         time.sleep(1)
         self.element_click("x","//*[@id='commonDialogWindow']/div/div/div[3]/button[1]")#点击确认保存
-        print u"点击保存，保存字段"
+        self.deprint(u"点击保存，保存字段")
     def new_custom_list_field(self,dic_name):
         time.sleep(1)
         self.element_click("id","_settingFelid")#点击属性/字段设置
         self.element_click("css","#con-basefield > div.m-tit-bar > div > div > button")#点击新增字段
-        print u"点击新增字段"
+        self.deprint(u"点击新增字段")
         self.element_value_input("x",'//*[@id="setFieldModal"]/div/div/div[2]/form/div[1]/div/input',u"自定义列表字段")#输入字段中文名称
-        print u"输入字段中文名称：自定义列表字段"
+        self.deprint(u"输入字段中文名称：自定义列表字段")
         self.element_value_input("x","//*[@id='setFieldModal']/div/div/div[2]/form/div[2]/div/input",u"zidingyiList")#输入字段英文名称
-        print u"输入字段中文名称：zidingyiList"
+        self.deprint( u"输入字段中文名称：zidingyiList")
         self.element_click("x", "//*[@id='setFieldModal']/div/div/div[2]/form/div[3]/div/div/button")#选择字段类型
         self.element_click("x", "//*[@id='setFieldModal']/div/div/div[2]/form/div[3]/div/div/ul/li[3]/a")#选择列表字段
-        print u"字段类型选择：列表字段"
+        self.deprint(u"字段类型选择：列表字段")
         self.element_click("x","//*[@id='setFieldModal']/div/div/div[2]/form/div[5]/div[2]/div[1]/div/button")#选择点击字典表
         try:
             for num in range(1,10000):#循环获取字典表名称
@@ -48,12 +49,12 @@ class FieldAction(BasePage):
                     time.sleep(1)
                     break#跳出循环
         except:
-            print u"未找到需要选取的字典表"
-        print u"字典表选择"+ dic_name
+            self.deprint(u"未找到需要选取的字典表")
+        self.deprint(u"字典表选择"+ dic_name)
         self.element_click("x","//*[@id='setFieldModal']/div/div/div[3]/button")#点击保存，保存创建的字段
         time.sleep(1)
         self.element_click("x","//*[@id='commonDialogWindow']/div/div/div[3]/button[1]")#点击确认保存
-        print u"新增列表字段成功"
+        self.deprint(u"新增列表字段成功")
     def del_field(self,field_name):
         #变量定义为基本字段的中文名称，传入中文名称即可删除对应字段
         time.sleep(1)
@@ -65,20 +66,20 @@ class FieldAction(BasePage):
                     self.element_click("x","//*[@id='con-basefield']/div[2]/div/div/table/tbody/tr["+ str(num) +"]/td[9]/a[2]")
                     time.sleep(1)
                     self.element_click("x","//*[@id='delModal']/div/div/div[3]/button[1]")
-                    print u"对应中文字删除成功"
+                    self.deprint(u"对应中文字删除成功")
                     break#跳出循环
         except:
-            print u"未找到需要删除的字段"
+            self.deprint(u"未找到需要删除的字段")
     def edit_form(self,form_name,choose_field1,choose_field2):
     #通过表单名称，找到表单，编辑表单，添加所有字段，删除所有字段，添加两个字段，设置为必填，然后保存
             self.element_click("x","/html/body/div[2]/div[1]/div/a[4]")#点击注册表单管理
             time.sleep(2)
             form_num = self.find_element_text("x","/html/body/div[2]/div[2]/div[1]/p/span[1]")#抓取表单数
-            print u"获得表单的总个数"
+            self.deprint(u"获得表单的总个数")
             for num in range(2,int(form_num)):
                 all_form_name = self.find_element_text("x","/html/body/div[2]/div[2]/div[2]/div["+ str(num) +"]/p")#获取表单名称
                 if all_form_name == form_name:#判断对应表单的名称，点击编辑按钮
-                    print u"点击编辑注册表单"
+                    self.deprint(u"点击编辑注册表单")
                     self.element_click("x","/html/body/div[2]/div[2]/div[2]/div["+ str(num) +"]/div[2]/a[2]/i")
                     break
             time.sleep(2)#编辑表单后，等待字段加载出来
@@ -86,7 +87,7 @@ class FieldAction(BasePage):
                 for sign_up_num in range(1,100):
                     self.element_click("x","/html/body/div[2]/div[1]/div[2]/div[1]/ul/li/ul/li["+ str(sign_up_num) +"]/a")
             except:
-                 print u"所有字段添加完毕"
+                 self.deprint(u"所有字段添加完毕")
             del_list = []#列一个需要删除字段数组的位置
             for all_filed_num in range(1,sign_up_num):
                 FieldName = self.find_element_AttributeText("x","/html/body/div[2]/div[1]/div[2]/div[2]/div/div["+ str(all_filed_num) +"]/div/div[1]/div/input","placeholder")
@@ -102,7 +103,7 @@ class FieldAction(BasePage):
                 ChooseFieldName  = self.find_element_text("x","/html/body/div[2]/div[1]/div[2]/div[1]/ul/li/ul/li["+ str(choose_num) +"]/a")
                 if ChooseFieldName == choose_field1 or ChooseFieldName == choose_field2:#判断字段名，添加相应的字段
                     self.element_click("x","/html/body/div[2]/div[1]/div[2]/div[1]/ul/li/ul/li["+ str(choose_num) +"]/a")
-            print u"添加两个字段："+choose_field1 + choose_field2
+            self.deprint(u"添加两个字段："+choose_field1 + choose_field2)
             for i in [2,3]:#选择第二个和第三个字段必填
                 try:
                     self.element_click("x","/html/body/div[2]/div[1]/div[2]/div[2]/div/div["+str(i)+"]/div/div[2]/div[2]/div[1]/label")
@@ -111,7 +112,7 @@ class FieldAction(BasePage):
             self.element_click("x","/html/body/div[2]/div[1]/div[2]/div[2]/div/p/a[2]")#点击保存按钮
             time.sleep(1)
             self.element_click("x","//*[@id='commonDialogWindow']/div/div/div[3]/button[1]")#点击确认按钮
-            print u"字段添加成功"
+            self.deprint(u"字段添加成功")
     def get_url_sign_up(self):
         # !/usr/bin/python3.4
         # -*- coding: utf-8 -*-
