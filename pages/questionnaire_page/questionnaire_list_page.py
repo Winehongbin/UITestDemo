afdb3f32cd71c19dc039579eac56a2e7773e4946
师经伟 <1017715951@qq.com>
2018-05-11 15:39:33 +0800

    等待显示并点击的方法改为：wait_is_visible

--- pages/questionnaire_page/questionnaire_list_page.py
+++ pages/questionnaire_page/questionnaire_list_page.py
@@ -10,7 +10,8 @@ class QuestionnaireListPage(BasePage):
     def open_questionBank(self):
         self.driver.switch_to.window(self.driver.window_handles[-1])  # 获取下一个窗口句柄，跳转
         self.deprint("点击管理题库")
-        self.dominant_wait('css','body > div.g-container-box > div.m-container.ng-scope > div.m-bar.ng-scope > div.pull-right > a.u-mr10')#点击管理题库的按钮
+        time.sleep(2)
+        self.find_element_click('css','body > div.g-container-box > div.m-container.ng-scope > div.m-bar.ng-scope > div.pull-right > a.u-mr10')#点击管理题库的按钮
 
 
 if __name__ == '__main__':
