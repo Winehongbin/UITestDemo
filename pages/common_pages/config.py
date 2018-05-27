f2c1c1dab286a2e92eb12e21c9507550770dfd26
高明 <cara_gao@sinobasedm.com>
2018-05-27 12:59:02 +0800

    错误截图

---
+++ pages/common_pages/config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#配置文件代码：
+def getconfig():
+    """
+       d = {"http://192.168.109.1:5555/wd/hub": "chrome",
+         "http://192.168.109.1:5556/wd/hub": "firefox"}
+    return d
+
+    """
+    d = {"http://192.168.99.100:4444/wd/hub": "chrome"}
+    return d
