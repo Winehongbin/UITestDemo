44f569c298792ee1b3872c5a77615a4acf5c846b
nina_xiao <nina_xiao@sinobasedm.com>
2018-07-13 16:59:16 +0800

    增加用例5

--- test_case/questionnaire_case.py
+++ test_case/questionnaire_case.py
@@ -45,7 +45,7 @@ class Questionnaire(unittest.TestCase):
         object = QuestionBankManagement(self.driver)
         object.delete_question()
 
-    '''def test_003_create_questionnaire(self):
+    def test_003_create_questionnaire(self):
          """创建常规问卷"""
          time.sleep(10)
          object=QuestionnaireListPage(self.driver)
@@ -53,7 +53,7 @@ class Questionnaire(unittest.TestCase):
          object=NewQuestionnairePage(self.driver)
          object.creat_new_questionnaire()
          time.sleep(5)
-         object.edit_questionnaire_subject()'''
+         object.edit_questionnaire_subject()
 
 if __name__ == '__main__':
     suit = unittest.TestSuite()
