1bfa98ba556fefac7540a811665546a502a0a730
测试刘雅 <1946898935@qq.com>
2018-07-23 17:37:54 +0800

    注释检测性能代码

--- test_case/base_unit.py
+++ test_case/base_unit.py
@@ -4,23 +4,23 @@
 import unittest
 from pages.common_pages.driver import brower
 from pages.common_pages.base import BasePage
-import cProfile
+# import cProfile
 class BaseUnit(unittest.TestCase):
 
     #setUp()方法用于测试用例执行前的初始化工作。
     def setUp(self):
         # 性能监控：测试用例执行时间
-        global cp,driver
-        cp = cProfile.Profile()
-        cp.enable()
+        # global cp,driver
+        # cp = cProfile.Profile()
+        # cp.enable()
         self.driver = brower()
 
     #tearDown()方法与setUp()相呼应，用于测试用例执行之后的善后工作
     def tearDown(self):
         # 性能监控：测试用例执行时间
         BasePage(self.driver).quit()
-        cp.disable()
-        cp.print_stats()
+        # cp.disable()
+        # cp.print_stats()
 
 
     # def setDown(self):
