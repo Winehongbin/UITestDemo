1270f3c35bacca5c915601ea2f0fe47c487f5ee4
Administrator <aurora_zhang@sinobasedm.com>
2018-08-15 10:37:57 +0800

    邮件发送任务增加

--- pages/management_tools/mail_pages/smsedm.py
+++ pages/management_tools/mail_pages/smsedm.py
@@ -3,6 +3,7 @@
 Created on 2018-05-10
 @author: 尤梅枝
 '''
+from selenium import webdriver
 from pages.common_pages.base import BasePage
 from pages.common_pages.login_page import LoginPage
 import time
@@ -39,6 +40,7 @@ class Edm_Sms(BasePage):
             return u'邮件创建失败'
 
     def list_edm(self):#20180809
+        time.sleep(4)
         self.driver.switch_to.window(self.driver.window_handles[-1])  # 获取下一个窗口句柄，跳转到邮件任务详情页面#20180809
         self.wait_is_visible('x','/html/body/div[1]/div[3]/div[1]/div[2]/div[2]/table/tbody/tr[1]/td[2]/a') #点击第一个邮件任务#20180809
 
@@ -71,33 +73,57 @@ class Edm_Sms(BasePage):
         self.scrollbar("bottom")
         self.wait_is_visible('x','/html/body/div[3]/div[2]/div[2]/form/div[6]/div/button') #点击保存按钮
 
-    #点击导入收件人#20180809
+    # 点击导入收件人#20180809
     def export(self):
-        # self.deprint("开始执行邮件任务发送用例")
-        # self.element_value_input('x', '/html/body/div[3]/div[2]/div[2]/div[4]/input', u'自动化创建')
-        # self.wait_is_visible('x', '/html/body/div[3]/div[2]/div[2]/button')  # 搜索自动化创建的邮件任务进行编辑
-        # for n in range(2, 5):
-        #     xpath = '/html/body/div[3]/div[2]/div[3]/table/tbody/tr[' + str(n) + ']/td[10]'
-        #     # print xpath
-        #     test = self.find_element_text('x', xpath)
-        #     if test == u'正常':
-        #         xpath2 = '/html/body/div[3]/div[2]/div[3]/table/tbody/tr[' + str(n) + ']/td[11]/a'
-        #         self.wait_is_visible('x', xpath2)
-        #         break
-        #     else:
-        #         continue
-        # self.driver.switch_to.window(self.driver.window_handles[-1])  # 获取下一个窗口句柄，跳转到邮件任务详情页面
-        # self.deprint("窗口切换成功")
-        self.wait_is_visible('x','/html/body/div[3]/div[2]/div[2]/div[1]/div/span')
-        self.wait_is_visible('x','//*[@id="importAddressee"]/div/div/div[2]/div/div[1]/div/button')
-        cur_path = os.path.abspath(os.path.dirname(__file__))
-        time.sleep(3)
-        os.system(cur_path + "/file/export.exe")
+        time.sleep(5)
+        self.driver.switch_to.window(self.driver.window_handles[-1])  # 获取下一个窗口句柄，跳转到邮件任务详情页面#20180809
+        self.wait_is_visible('x', '/html/body/div[1]/div[3]/div[1]/div[1]/ul/li[3]/div[2]/button')
+        self.wait_is_visible('x', '//*[@id="importAddressee"]/div/div/div[2]/div/div[2]/div[2]/button')
+        cur_path = os.path.abspath(os.path.dirname(__file__)) + "\\file\export.exe"
+        zh_path = eval(repr(cur_path).replace('\\', '\\\\'))
+        time.sleep(2)
+        os.system(zh_path)
         self.deprint("收件人上传成功")
-        self.wait_is_visible('x','//*[@id="importAddressee"]/div/div/div[3]/button')
+        self.wait_is_visible('x', '//*[@id="importAddressee"]/div/div/div[3]/button')
+        time.sleep(2)
         self.deprint("开始导入收件人")
         time.sleep(10)
         # self.driver.quit()
+
+    def editMail(self):
+        self.deprint("开始执行邮件任务编辑用例")
+        time.sleep(3)
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+        self.wait_is_visible('x',
+                             '/html/body/div[1]/div[3]/div[1]/div[2]/div/div[7]/div[1]/div[2]/ul/li[3]/a')  # 点击编辑按钮
+        self.deprint("点击编辑按钮成功")
+        iframe = self.driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[8]/iframe")
+        self.driver.switch_to_frame(iframe)
+        ifr = self.driver.find_element_by_xpath('//*[@id="ueditor_0"]')
+        self.driver.switch_to.frame(ifr)  # 切换iframe到邮件内容输入窗口
+        self.deprint("iframe切换成功")
+        self.wait_is_visible('x', '/html/body')
+        self.element_value_input('x', '/html/body', u'自动化测试编辑的内容')  # 输入邮件内容
+        self.driver.switch_to.parent_frame()  # 从邮件内容输入的iframe窗口切换回主文档
+        self.deprint("从iframe切回主文档成功")
+        self.scrollbar("bottom")
+        self.wait_is_visible('x', '/html/body/div[1]/div[1]/form/div[7]/div/input')  # 点击保存按钮
+
+    def immeSendMail(self):
+        time.sleep(5)
+        self.driver.switch_to.window(self.driver.window_handles[-1])  # 获取下一个窗口句柄，跳转到邮件任务详情页面#20180809
+        self.wait_is_visible('x', '/html/body/div[1]/div[3]/div[1]/div[2]/div/div[1]/div[2]/span[3]')  # 进入发送任务管理
+        self.deprint("进入发送任务管理")
+        time.sleep(7)
+        iframe = self.driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[8]/iframe")
+        self.driver.switch_to_frame(iframe)
+        self.wait_is_visible('x', '/html/body/div[1]/section/div/div[1]/div[2]/button[2]')  # 点击启动发送按钮]
+        self.deprint("启动发送")
+        self.wait_is_visible('x', '//*[@id="TaskSend"]/div/div/div[2]/div[2]/label/ins')  # 选中立即执行发送按钮
+        self.wait_is_visible('x', '//*[@id="TaskSend"]/div/div/div[3]/button[2]')  # 点击确定按钮
+        time.sleep(5)
+        self.wait_is_visible('x', '//*[@id="alertCommon"]/div/div/div[3]/button')  # 点击确定按钮
+
 
 
 
@@ -113,10 +139,13 @@ if __name__ == '__main__':
     S=Edm_Sms(dr)
     S.createEdm(type)
     S.list_edm()
-    e = Details_Edm(dr)
-    e.export_edm()
+    #e = Details_Edm(dr)
+    S.editMail()
+    S.export()
+    S.immeSendMail()
+    #e.export_edm()
 
     # S.editEdm()
-    # S.export()
+    #S.export()
 
 
