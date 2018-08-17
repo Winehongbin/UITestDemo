ef52fa751059e4f4a9852704c323420f66d370ad
高明13393213135 <cara_gao@sinobasedm.com>
2018-08-17 11:59:30 +0800

    栏目用例1调试

--- test_case/article_case.py
+++ test_case/article_case.py
@@ -6,6 +6,7 @@ from pages.common_pages.driver import brower
 from pages.common_pages.login_page import LoginPage
 from pages.common_pages.choose_page import ChoosePage
 from pages.management_tools.article_pages.section_list_page import SectionListPage
+from pages.management_tools.article_pages.browse_page import BrowsePage
 
 class Article(unittest.TestCase):
     """ 文章管理测试用例（创建栏目） """
@@ -22,11 +23,18 @@ class Article(unittest.TestCase):
 
     def test_001_create_section(self):
         """创建浏览删除栏目"""
-        time.sleep(10)
-        object = SectionListPage(self.driver)
-        object.new_section()
-        object.browse_section()
-        actual_result = object.open_detail_data()
+        dr = brower()
+        object = LoginPage(dr)
+        object.login()
+        object = ChoosePage(dr)
+        object.click_menu_bt('22')
+        o = SectionListPage(dr)
+        o.new_section()
+        o.choose_more()
+        o = BrowsePage(dr)
+        o.browse_section()
+        o = SectionListPage(dr)
+        actual_result = o.open_detail_data()
         expected_result = (u'1',u'1')
         # 对元祖进行断言
         self.assertTupleEqual(actual_result,expected_result,"fail")
