8f9ff31e3c85aa2c14181d0a3e47b031d28e90dc
Administrator <aurora_zhang@sinobasedm.com>
2018-08-19 23:58:54 +0800

    邮件发送任务增加8

--- pages/management_tools/mail_pages/smsedm.py
+++ pages/management_tools/mail_pages/smsedm.py
@@ -90,7 +90,7 @@ class Edm_Sms(BasePage):
         zh_path = eval(repr(sp_path+con_path).replace('\\', '\\\\'))
         self.deprint(u"上传的文件地址："+zh_path )
         os.system(zh_path)
-        time.sleep(20)
+        time.sleep(60)
         fname = self.find_element_text('x', '//*[@id="importAddressee"]/div/div/div[2]/div/div[3]/div')
         self.deprint(u"上传的文件名称：" + fname)
         scwb = self.find_element_text('x', '//*[@id="importAddressee"]/div/div/div[2]/div/div[2]/div[1]/input')
