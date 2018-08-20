dac05143e8f75ba10662d97a234eb2846ca66a77
Administrator <aurora_zhang@sinobasedm.com>
2018-08-20 10:18:43 +0800

    邮件发送任务增加15

--- test_case/article_case.py
+++ test_case/article_case.py
@@ -21,32 +21,32 @@ class Article(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-    def test_001_create_section(self):
-        """创建浏览删除栏目"""
-
-        o = SectionListPage(self.driver)
-        # o.new_section()
-        o.choose_more()
-        p = BrowsePage(self.driver)
-        p.browse_section()
-        time.sleep(3)
-        # o = SectionListPage(self.driver)
-        # o.browse_section()
-        o = SectionListPage(self.driver)
-        # actual_result = o.open_detail_data()
-        # print  actual_result
-        # expected_result = (u'1', u'1')
-        # # 对元祖进行断言
-        # self.assertTupleEqual(actual_result, expected_result, "fail")
-        # self.assertEqual(actual_result, expected_result, msg="failed")  #验证浏览量和浏览人数
-        # object.deprint("浏览量和浏览人数验证通过")
-        dickname = o.open_detail_data()
-        expect_result ={'name1:' : u"1" , 'name2:': u"1"}
-        self.assertDictEqual(expect_result, dickname, u"浏览量和浏览人数验证不通过")#验证浏览量和浏览人数
+    # def test_001_create_section(self):
+    #     """创建浏览删除栏目"""
+    #
+    #     o = SectionListPage(self.driver)
+    #     # o.new_section()
+    #     o.choose_more()
+    #     p = BrowsePage(self.driver)
+    #     p.browse_section()
+    #     time.sleep(3)
+    #     # o = SectionListPage(self.driver)
+    #     # o.browse_section()
+    #     o = SectionListPage(self.driver)
+    #     # actual_result = o.open_detail_data()
+    #     # print  actual_result
+    #     # expected_result = (u'1', u'1')
+    #     # # 对元祖进行断言
+    #     # self.assertTupleEqual(actual_result, expected_result, "fail")
+    #     # self.assertEqual(actual_result, expected_result, msg="failed")  #验证浏览量和浏览人数
+    #     # object.deprint("浏览量和浏览人数验证通过")
+    #     dickname = o.open_detail_data()
+    #     expect_result ={'name1:' : u"1" , 'name2:': u"1"}
+    #     self.assertDictEqual(expect_result, dickname, u"浏览量和浏览人数验证不通过")#验证浏览量和浏览人数
 
 
 if __name__ == '__main__':
     suit = unittest.TestSuite()
-    suit.addTest(Article("test_001_create_section"))
+    #suit.addTest(Article("test_001_create_section"))
     runner = unittest.TextTestRunner()
     runner.run(suit)
